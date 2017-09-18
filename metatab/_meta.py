6cff571a56fb046618597451e15962bd662a437c
Eric Busboom <eric@clarinova.com>
2017-09-18 15:23:04 -0700

    Rev

--- metatab/_meta.py
+++
@@ -1,2 +0,0 @@
-__version__ = '0.6.5'
-__author__ = 'eric@civicknowledge.com'
--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import imp
+
 import os
 import sys
-
-from os.path import  join
-
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
@@ -19,8 +12,6 @@ if sys.argv[-1] == 'publish':
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
     readme = f.read()
-
-ps_meta = imp.load_source('_meta', 'metatab/_meta.py')
 
 classifiers = [
     'Development Status :: 4 - Beta',
@@ -36,18 +27,16 @@ classifiers = [
 
 setup(
     name='metatab',
-    version=ps_meta.__version__,
+    version='0.6.6',
     description='Data format for storing structured data in spreadsheet tables',
     long_description=readme,
-    packages=['metatab'],
+    packages=['metatab','metatab.templates'],
 
     install_requires=[
-        'appurl',
-        'six', # Should be removed. Not supporting python 2.7 anymore.
-        'rowgenerators>=0.4.3',
+        'six',  # Should be removed. Not supporting python 2.7 anymore.
         'metatabdecl',
-        'deprecation'
-
+        'appurl>=0.1.5',
+        'rowgenerators>=0.7.0',
     ],
 
     entry_points={
@@ -59,14 +48,12 @@ setup(
 
     include_package_data=True,
 
-    author=ps_meta.__author__,
-    author_email=ps_meta.__author__,
+    author='Eric Busboom',
+    author_email='eric@civicknowledge.com',
     url='https://github.com/CivicKnowledge/metatab-py.git',
     license='BSD',
     classifiers=classifiers,
     extras_require={
        'datapackage': ['datapackage'],
-       # 'geo': ['fiona','shapely','pyproj'],
-
     }
 )
