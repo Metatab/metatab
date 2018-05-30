13fb3d2e9623b0466e46e0f746502ba4709fc8e0
Eric Busboom <eric@civicknowledge.com>
2018-05-30 12:54:10 -0700

    Better ipy handing

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ classifiers = [
 
 setup(
     name='metatab',
-    version='0.7.2',
+    version='0.7.3',
     description='Data format for storing structured data in spreadsheet tables',
     long_description=readme,
     packages=['metatab','metatab.templates', 'metatab.test', 'metatab.test.test-data'],
