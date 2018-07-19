5b3b149c4b48b05a6d931fc9d72d674a151a0fb7
Eric Busboom <eric@civicknowledge.com>
2018-07-19 12:38:48 -0700

    Updated classifiers

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ classifiers = [
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
