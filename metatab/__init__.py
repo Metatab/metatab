0b98efe70c89ea9339423c2d284546f71e5b35fd
Eric Busboom <eric@civicknowledge.com>
2018-09-18 11:57:42 -0700

    more test fixing

--- metatab/__init__.py
+++ metatab/__init__.py
@@ -11,3 +11,10 @@ from .exc import *
 from .doc import MetatabDoc
 from .resolver import WebResolver
 
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
