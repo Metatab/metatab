f169506e1e40c4220123be149e249bcaecd79be1
Eric Busboom <eric@civicknowledge.com>
2018-09-17 21:54:33 -0700

    rev

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ classifiers = [
 
 setup(
     name='metatab',
-    version='0.7.9',
+    version='0.7.10',
     description='Data format for storing structured data in spreadsheet tables',
     long_description=readme,
     packages=['metatab','metatab.templates', 'metatab.test', 'metatab.test.test-data'],
